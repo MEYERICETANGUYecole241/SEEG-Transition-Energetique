93999064459dd605db644ae6b02fa625f24b0e8c
ICE Tanguy <icetanguy065@gmail.com>
2025-03-18 23:10:52 +0100

    Initialisation du projet SEEG - Transition énergétique

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,7 @@
-document.querySelector('.cta-button').addEventListener('click', function() {
-    const infosSupp = document.getElementById('infos-supplementaires');
-    const formulaireInscription = document.getElementById('formulaire-inscription');
-
-    if (this.textContent === "En savoir plus") {
-        infosSupp.style.display = 'block';  // Affiche les informations supplémentaires
-    } else if (this.textContent === "S'inscrire") {
-        formulaireInscription.style.display = 'block';  // Affiche le formulaire d'inscription
-    }
-});
+export default {
+    content: ["./*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
